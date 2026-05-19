82fbf605f9134e320c4ba59ad38c35d086a28df6
TehBrian <tehbrian@proton.me>
2026-05-19 18:45:31 -0400

    update deps

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
 	api(project(":agna-configurate"))
-	compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+	compileOnly("io.papermc.paper:paper-api:26.1.2.build.64-stable")
 }
