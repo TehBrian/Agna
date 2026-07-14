0950c278a1631dd231044a015c85fdf84174e418
TehBrian <tehbrian@proton.me>
2026-07-14 15:47:26 -0400

    update deps

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,11 +9,14 @@ repositories {
 
 dependencies {
 	api(project(":agna-configurate"))
+
 	compileOnly("com.google.code.gson:gson:2.14.0")
-	compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
-	testImplementation(platform("org.junit:junit-bom:6.1.0"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("com.google.code.gson:gson:2.14.0")
-	testRuntimeOnly("io.papermc.paper:paper-api:26.1.2.build.65-stable")
+
+	compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+	testRuntimeOnly("io.papermc.paper:paper-api:26.2.build.+")
+
+	testImplementation(platform("org.junit:junit-bom:6.1.2"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
