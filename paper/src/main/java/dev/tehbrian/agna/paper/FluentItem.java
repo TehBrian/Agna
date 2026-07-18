4584519c64a5c830b51a944f6626cfce4b866525
TehBrian <tehbrian@proton.me>
2026-07-17 21:48:05 -0400

    ItemEditor → FluentItem + improvements
    - improved static factories
    - made cloning explicit
    - added PDC shit
    - add regions yay

--- paper/src/main/java/dev/tehbrian/agna/paper/FluentItem.java
+++ paper/src/main/java/dev/tehbrian/agna/paper/FluentItem.java
@@ -11,9 +11,11 @@ import io.papermc.paper.datacomponent.item.TooltipDisplay;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Color;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
+import org.bukkit.persistence.PersistentDataType;
 import org.jspecify.annotations.Nullable;
 
 import java.util.List;
@@ -23,38 +25,38 @@ import java.util.function.UnaryOperator;
 import static com.google.common.base.Predicates.alwaysTrue;
 
 @SuppressWarnings("UnstableApiUsage")
-public final class ItemEditor {
+public final class FluentItem {
 
 	private static final Component DISABLE_ITALICS = Component.empty().decoration(TextDecoration.ITALIC, false);
 
 	private ItemStack item;
 
-	private ItemEditor(final ItemStack item) {
-		this.item = item.clone();
+	private FluentItem(final ItemStack item) {
+		this.item = item;
 	}
 
-	private ItemEditor(final ItemType type) {
-		this.item = type.createItemStack();
+	public static FluentItem editItem(final ItemStack item) {
+		return new FluentItem(item);
 	}
 
-	public static ItemEditor edit(final ItemStack item) {
-		return new ItemEditor(item);
+	public static FluentItem cloneItem(final ItemStack item) {
+		return new FluentItem(item.clone());
 	}
 
-	public static ItemEditor edit(final ItemType type) {
-		return new ItemEditor(type);
+	public static FluentItem createItem(final ItemType type) {
+		return new FluentItem(type.createItemStack());
 	}
 
 	public ItemStack item() {
 		return this.item;
 	}
 
-	public ItemEditor amount(final int amount) {
+	public FluentItem amount(final int amount) {
 		this.item.setAmount(amount);
 		return this;
 	}
 
-	public ItemEditor type(final ItemType type) {
+	public FluentItem type(final ItemType type) {
 		final var repl = type.createItemStack();
 		repl.copyDataFrom(this.item, alwaysTrue());
 		repl.setAmount(this.item.getAmount());
@@ -62,7 +64,9 @@ public final class ItemEditor {
 		return this;
 	}
 
-	public ItemEditor set(final DataComponentType.NonValued type) {
+	//region data components
+
+	public FluentItem set(final DataComponentType.NonValued type) {
 		this.item.setData(type);
 		return this;
 	}
@@ -71,22 +75,22 @@ public final class ItemEditor {
 		return this.item.getData(type);
 	}
 
-	public <T> ItemEditor set(final DataComponentType.Valued<T> type, final T data) {
+	public <T> FluentItem set(final DataComponentType.Valued<T> type, final T data) {
 		this.item.setData(type, data);
 		return this;
 	}
 
-	public ItemEditor unset(final DataComponentType type) {
+	public FluentItem unset(final DataComponentType type) {
 		this.item.unsetData(type);
 		return this;
 	}
 
-	public ItemEditor reset(final DataComponentType type) {
+	public FluentItem reset(final DataComponentType type) {
 		this.item.resetData(type);
 		return this;
 	}
 
-	public <T> ItemEditor update(
+	public <T> FluentItem update(
 			final DataComponentType.Valued<T> type,
 			final UnaryOperator<T> operator
 	) {
@@ -97,30 +101,86 @@ public final class ItemEditor {
 		return this.set(type, operator.apply(current));
 	}
 
-	public <T> ItemEditor compute(
+	public <T> FluentItem compute(
 			final DataComponentType.Valued<T> type,
 			final Function<@Nullable T, T> operator
 	) {
 		return this.set(type, operator.apply(this.get(type)));
 	}
 
-	public ItemEditor itemName(final Component data) {
+	//endregion
+
+	//region PDC
+
+	/**
+	 * Gets persistent data from the item's {@link org.bukkit.persistence.PersistentDataContainer}.
+	 *
+	 * @param key  the {@code NamespacedKey} to use
+	 * @param type the {@code PersistentDataType to use}
+	 * @param <T>  the primary object type of the data
+	 * @param <Z>  the retrieve object type of the data
+	 * @return the data
+	 */
+	public <T, Z> @Nullable Z pdcGet(
+			final NamespacedKey key,
+			final PersistentDataType<T, Z> type
+	) {
+		return this.item.getPersistentDataContainer().get(key, type);
+	}
+
+	/**
+	 * Adds persistent data to the item's {@link org.bukkit.persistence.PersistentDataContainer}.
+	 *
+	 * @param key    the {@code NamespacedKey} to use
+	 * @param type   the {@code PersistentDataType} to use
+	 * @param object the data to set
+	 * @param <T>    the primary object type of the data
+	 * @param <Z>    the retrieve object type of the data
+	 * @return the builder
+	 */
+	public <T, Z> FluentItem pdcSet(
+			final NamespacedKey key,
+			final PersistentDataType<T, Z> type,
+			final Z object
+	) {
+		this.item.editPersistentDataContainer(c -> c.set(key, type, object));
+		return this;
+	}
+
+	/**
+	 * Removes persistent data from the item's {@link org.bukkit.persistence.PersistentDataContainer}.
+	 *
+	 * @param key the {@code NamespacedKey} to use
+	 * @return the builder
+	 */
+	public FluentItem pdcRemove(
+			final NamespacedKey key
+	) {
+		this.item.editPersistentDataContainer(c -> c.remove(key));
+		return this;
+	}
+
+	//endregion
+
+	// region data component helper utilities
+
+	public FluentItem itemName(final Component data) {
 		this.set(DataComponentTypes.ITEM_NAME, data);
 		return this;
 	}
 
-	public ItemEditor customName(final Component data) {
+	public FluentItem customName(final Component data) {
 		this.set(DataComponentTypes.CUSTOM_NAME, data);
 		return this;
 	}
 
-	public ItemEditor lore(final List<Component> data) {
+	public FluentItem lore(final List<Component> data) {
 		// sidestep default formatting by appending each component to a dummy
 		this.set(DataComponentTypes.LORE, ItemLore.lore(data.stream().map(DISABLE_ITALICS::append).toList()));
 		return this;
 	}
 
-	public ItemEditor textures(final String data) {
+	public FluentItem textures(final String data) {
 		this.set(
 				DataComponentTypes.PROFILE,
 				ResolvableProfile.resolvableProfile().addProperty(
@@ -130,19 +190,21 @@ public final class ItemEditor {
 		return this;
 	}
 
-	public ItemEditor bannerPatterns(final List<Pattern> data) {
+	public FluentItem bannerPatterns(final List<Pattern> data) {
 		this.set(DataComponentTypes.BANNER_PATTERNS, BannerPatternLayers.bannerPatternLayers(data));
 		return this;
 	}
 
-	public ItemEditor dyedColor(final Color data) {
+	public FluentItem dyedColor(final Color data) {
 		this.set(DataComponentTypes.DYED_COLOR, DyedItemColor.dyedItemColor(data));
 		return this;
 	}
 
-	public ItemEditor hideTooltip(final boolean data) {
+	public FluentItem hideTooltip(final boolean data) {
 		this.set(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplay.tooltipDisplay().hideTooltip(data).build());
 		return this;
 	}
+
+	// endregion
 
 }
